3dada5154c829938a24a6a94a2708d7ac61d20c9
unknown <marianobalma@hotmail.com>
2023-05-02 10:21:30 -0300

    componentes y hooks

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Home } from "./components/pages/home/Home";
+
+// otra forma de importar
+//import Home from "./components/pages/home/Home";
+
+//formas de ejecutar una function en js--- Home()
+// otra forma--- <Home />
+
+/*let suma = ()=>{
+  let num1 = 5
+  let num2 = 10
+
+  return num1 + num2
+};
+
+let resultado = suma()*/ //15
+
+function App() {
+  return (
+    <div className="App">
+      <Home /> 
+    </div>
+  );
+}
+
+export default App;
