9af8c279efe4a406e1ed2d6635dc070ad5e129e3
marianobalma <marianobalma@hotmail.com>
2023-05-05 10:34:53 -0300

    Formulario_terminado

--- src/components/pages/formulario/Formulario.jsx
+++ src/components/pages/formulario/Formulario.jsx
@@ -1,27 +1,76 @@
 import * as React from "react";
-import {Grid,TextField,Typography} from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import "./Formulario.css";
+import { useFormik } from "formik";
+import * as Yup from "yup"
 
 const Formulario = () => {
+  /*let initialValues ={
+    nombre:"",
+    email:"",
+    contraseña:""
+  }*/
+
+  const enviarForm = (data) => {
+    console.log(data);
+  };
+
+  const {handleSubmit, handleChange, values, errors} = useFormik({
+    initialValues: {
+      nombre: "",
+      email: "",
+      contraseña: "",
+    },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required("Debes ingresar un nombre"),
+      email: Yup.string().required("Debes ingresar un email"),
+      contraseña: Yup.string().required("Debes ingresar una contraseña"),
+    }) ,
+
+    // esto obliga al usuario a completar todos los campos
+
+    onSubmit: enviarForm,
+  });
+// se puede desestructurar formik para no escribir x ej formik.handleSubmit
   return (
     <div>
-      <Typography color="primary">Formulario</Typography>
-      <form action="" className="form-container">
+      <Typography color="primary" variant="h2" align="center">
+        Formulario
+      </Typography>
+      <form className="form-container" onSubmit={handleSubmit}>
         <Grid
           container
           align-items={"center"}
           justify-content="space-evenly"
-          spacing ={2}
-          sx= {{width: "100%"}}
+          spacing={2}
+          sx={{ width: "100%" }}
         >
           <Grid item xs={12} md={7}>
-            <TextField type="text" label="Ingrese nombre" variant="outlined"
-            fullWidth />
+            <TextField
+              type="text"
+              label="Ingrese nombre"
+              variant="outlined"
+              fullWidth
+              name="nombre"
+              onChange={handleChange}
+              value={values.nombre}
+              error={errors.nombre}
+              helperText={errors.nombre}
+            />
           </Grid>
 
           <Grid item xs={12} md={7}>
-            <TextField type="email" label="Ingrese email" variant="outlined"
-            fullWidth />
+            <TextField
+              type="email"
+              label="Ingrese email"
+              variant="outlined"
+              fullWidth
+              name="email"
+              onChange={handleChange}
+              value={values.email}
+              error={errors.email}
+              helperText={errors.email}
+            />
           </Grid>
 
           <Grid item xs={12} md={7}>
@@ -30,9 +79,17 @@ const Formulario = () => {
               label="Ingrese contraseña"
               variant="outlined"
               fullWidth
+              name="contraseña"
+              onChange={handleChange}
+              value={values.contraseña}
+              error={errors.contraseña}
+              helperText={errors.contraseña}
             />
           </Grid>
         </Grid>
+        <Button type="submit" variant="contained">
+          Enviar
+        </Button>
       </form>
     </div>
   );
