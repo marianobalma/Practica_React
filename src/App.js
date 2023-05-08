a3ffcf5e71c0bdba2d8908b8f2bcf1742656e6a0
marianobalma <marianobalma@hotmail.com>
2023-05-08 00:56:17 -0300

    seguir 7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import { Home } from "./components/pages/home/Home";
-
-// otra forma de importar
-//import Home from "./components/pages/home/Home";
-
-//formas de ejecutar una function en js--- Home()
-// otra forma--- <Home />
-
-/*let suma = ()=>{
-  let num1 = 5
-  let num2 = 10
-
-  return num1 + num2
-};
-
-let resultado = suma()*/ //15
-
 function App() {
   return (
     <div className="App">
-      <Home /> 
     </div>
   );
 }
